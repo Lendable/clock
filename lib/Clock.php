39083d5df2b30d6a5878d0541a3fe019f189ac01
Fernando Cardona <fernando.cardona@lendable.co.uk>
2020-10-08 16:24:44 +0100

    [BOYSCOUT] public const for microseconds datetime format

--- lib/Clock.php
+++ lib/Clock.php
@@ -6,6 +6,8 @@ namespace Lendable\Clock;
 
 interface Clock
 {
+    public const MICROSECONDS_DATETIME_FORMAT = 'Y-m-d H:i:s.u';
+
     public function now(): \DateTimeImmutable;
 
     public function nowMutable(): \DateTime;
