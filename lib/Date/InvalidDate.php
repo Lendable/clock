ffa7f5416680373ec8cdf40b1c82c254eb0bfc82
Sam Burns <Sam-Burns@users.noreply.github.com>
2021-07-30 13:50:23 +0100

    Make class final
    Co-authored-by: Ben Challis <ben-challis@users.noreply.github.com>

--- lib/Date/InvalidDate.php
+++ lib/Date/InvalidDate.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Lendable\Clock\Date;
 
-class InvalidDate extends \InvalidArgumentException
+final class InvalidDate extends \InvalidArgumentException
 {
     public static function fromDate(int $year, int $month, int $day): self
     {
