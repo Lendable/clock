693b93e2663c6419d809c98b6588ed2bc47c59cb
Ben Challis <ben-challis@users.noreply.github.com>
2021-08-30 11:06:04 +0100

    Ignore DateTimeToDateTimeInterfaceRector as treating mutable vs immutable in same fashion is dangerous (#64)
    Co-authored-by: Martin Georgiev <martin-georgiev@users.noreply.github.com>

--- rector.php
+++ rector.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Rector\CodeQuality\Rector\ClassMethod\DateTimeToDateTimeInterfaceRector;
 use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\Set\ValueObject\SetList;
@@ -12,6 +13,9 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters->set(Option::PATHS, [__DIR__.'/lib', __DIR__.'/tests']);
     $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_74);
     $parameters->set(Option::PHPSTAN_FOR_RECTOR_PATH, __DIR__.'/phpstan.neon');
+    $parameters->set(Option::SKIP, [
+        DateTimeToDateTimeInterfaceRector::class,
+    ]);
 
     $containerConfigurator->import(SetList::CODE_QUALITY);
     $containerConfigurator->import(SetList::PHP_74);
