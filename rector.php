821cc6ea45cde6d4c6080f4dcdb673a4a7f228ac
Ben Challis <ben-challis@users.noreply.github.com>
2022-03-07 16:35:36 +0000

    Use LevelSetList::UP_TO_PHP_* @ Rector (#189)

--- rector.php
+++ rector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Rector\CodeQuality\Rector\ClassMethod\DateTimeToDateTimeInterfaceRector;
 use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
+use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
@@ -19,6 +20,5 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     ]);
 
     $containerConfigurator->import(SetList::CODE_QUALITY);
-    $containerConfigurator->import(SetList::PHP_74);
-    $containerConfigurator->import(SetList::PHP_80);
+    $containerConfigurator->import(LevelSetList::UP_TO_PHP_80);
 };
