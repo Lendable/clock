5e59460b8d21af31f33d8cb0f83565e305908371
Ben Challis <ben-challis@users.noreply.github.com>
2022-04-21 11:09:20 +0100

    Migrate to new RectorConfig style of configuring Rector (#263)

--- rector.php
+++ rector.php
@@ -3,23 +3,21 @@
 declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\ClassMethod\DateTimeToDateTimeInterfaceRector;
-use Rector\Core\Configuration\Option;
+use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PARALLEL, true);
-    $parameters->set(Option::PATHS, [__DIR__.'/lib', __DIR__.'/tests']);
-    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_80);
-    $parameters->set(Option::PHPSTAN_FOR_RECTOR_PATH, __DIR__.'/phpstan-rector.neon');
-    $parameters->set(Option::PARALLEL, true);
-    $parameters->set(Option::SKIP, [
+return static function (RectorConfig $rector): void {
+    $rector->parallel();
+    $rector->paths([__DIR__.'/lib', __DIR__.'/tests']);
+    $rector->phpVersion(PhpVersion::PHP_80);
+    $rector->phpstanConfig(__DIR__.'/phpstan-rector.neon');
+    $rector->skip([
         DateTimeToDateTimeInterfaceRector::class,
     ]);
-
-    $containerConfigurator->import(SetList::CODE_QUALITY);
-    $containerConfigurator->import(LevelSetList::UP_TO_PHP_80);
+    $rector->sets([
+        SetList::CODE_QUALITY,
+        LevelSetList::UP_TO_PHP_80,
+    ]);
 };
