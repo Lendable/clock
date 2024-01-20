5feefae397d16861f85f7d0cf88aef1a6dfb02fd
Marcin Michalski <marcin.michalski@lendable.co.uk>
2024-01-20 21:51:55 +0000

    Analyse rector config with phpstan

--- rector.php
+++ rector.php
@@ -3,10 +3,10 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Core\ValueObject\PhpVersion;
 use Rector\PHPUnit\Set\PHPUnitLevelSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
+use Rector\ValueObject\PhpVersion;
 
 return static function (RectorConfig $rector): void {
     $rector->parallel();
