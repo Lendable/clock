7e4642242eee44a6bcfbc6ea0da59984a9e9027f
Ben Challis <ben@lendable.co.uk>
2024-02-19 09:19:29 +0000

    Use new Rector config method

--- rector.php
+++ rector.php
@@ -4,20 +4,12 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\PHPUnit\Set\PHPUnitSetList;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
-use Rector\ValueObject\PhpVersion;
 
-return static function (RectorConfig $rector): void {
-    $rector->parallel();
-    $rector->paths([__DIR__.'/lib', __DIR__.'/tests']);
-    $rector->phpVersion(PhpVersion::PHP_81);
-    $rector->phpstanConfig(__DIR__.'/phpstan-rector.neon');
-    $rector->importNames();
-    $rector->importShortClasses(false);
-    $rector->sets([
-        SetList::CODE_QUALITY,
-        LevelSetList::UP_TO_PHP_81,
-        PHPUnitSetList::PHPUNIT_100,
-    ]);
-};
+return RectorConfig::configure()
+    ->withPaths([__DIR__.'/lib', __DIR__.'/tests'])
+    ->withRootFiles()
+    ->withPHPStanConfigs([__DIR__.'/phpstan-rector.neon'])
+    ->withPhpSets(php81:true)
+    ->withPreparedSets(codeQuality: true)
+    ->withAttributesSets(phpunit: true)
+    ->withSets([PHPUnitSetList::PHPUNIT_100]);
